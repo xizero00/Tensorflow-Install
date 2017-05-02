a747e52e41f9de57df904ba2f6d4cb259c05b13b
xizero00 <xizero00@gmail.com>
2017-05-02 11:18:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Tensorflow installing script
+
+## Introduction
+This scrip install tensorflow-gpu automatically, it can install
+ipython2 
+ipython3
+pip2
+pip3
+tensorflow-gpu in python2
+tensorflow-gpu in python3
+
+in Ubuntu 14.04 and 16.04(tested)
+
+
+## Settings
+If you have proxy such as shadowsocks you can set 
+
+USE_PROXYCHAIN=1
+
+and set the proxy server's protocol, ip and port
+
+PROXY_SERVER='http\t127.0.0.1:1080'
+
+or 
+
+PROXY_SERVER='socks\t127.0.0.1:1080'
+
+or
+
+PROXY_SERVER='https\t127.0.0.1:1080'
+
+If you do not have proxy you can set pip souce
+
+Tsinghua pip source
+
+PIP_SOURCE='https://pypi.tuna.tsinghua.edu.cn/simple'
+
+or USTC pip source
+
+PIP_SOURCE='https://mirrors.ustc.edu.cn/pypi/web/simple'
+
+
+here I set the pip source to tsinghua university pip source
+
+## Usage
+sudo bash install-tensorflow.sh
+
+
+## Others
+Please feel free to issue problems
---
+++ install-tensorflow.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# author xizero00
+
+# if you have proxy such as shadowsocks you can set proxy settings
+# if USE_PROXYCHAIN=1 then it will use proxychains to install tensorflow-gpu
+# you have to set proxy server address
+# else it will use tsinghua university pip source to install tensorflow-gpu
+USE_PROXYCHAIN=
+PROXY_SERVER='http\t127.0.0.1:1080'
+PIP_SOURCE='https://pypi.tuna.tsinghua.edu.cn/simple'
+
+if [ $USE_PROXYCHAIN ]
+then
+
+	# setup proxychains
+	sudo apt -y  install proxychains
+
+	# check whether proxychains is running
+	existpid=`ps -al | grep "proxychains"|wc -l`
+	if [ -gt $existpid ]
+	then
+		pid=`ps -al |grep "proxychains" | awk '{print $4}'`
+		sudo kill -9 ${pid}
+	fi
+
+	# check whether conf file exists
+	if [ -f "/etc/proxychains.conf" ]; then
+		sudo mv /etc/proxychains.conf /etc/proxychains.conf.bak
+	fi
+
+	# write conf file
+	echo -e "strict_chain\nproxy_dns\ntcp_read_time_out 15000\ntcp_connect_time_out 8000\n[ProxyList]\n${PROXY_SERVER}" |sudo tee /etc/proxychains.conf
+
+	#sudo echo -e "strict_chain\nproxy_dns\ntcp_read_time_out 15000\ntcp_connect_time_out 8000\n[ProxyList]\nhttp\t127.0.0.1:1080" > /etc/proxychains.conf
+
+
+	# install in python3
+	sudo apt -y install ipython3
+	sudo apt -y install python3-pip
+
+	sudo -H proxychains pip3 install -U pip
+	sudo apt -y remove python3-pip
+
+	sudo -H proxychains pip3 install -U tensorflow-gpu
+
+	# install in python2
+	sudo apt -y install ipython
+	sudo apt -y install python-pip
+	sudo -H proxychains pip install -U pip
+	sudo apt -y remove python-pip
+
+	sudo -H proxychains pip install -U tensorflow-gpu
+	
+	echo 'check python2 dependency'
+	sudo -H proxychains pip install -i check
+	echo 'check python3 dependency'
+	sudo -H proxychains pip3 install -i check
+	
+else
+	# using tsinghua university pip source
+	# install in python3
+	sudo apt -y install ipython3
+	sudo apt -y install python3-pip
+
+	sudo -H pip3 install -i ${PIP_SOURCE} -U pip
+	sudo apt -y remove python3-pip
+	
+	sudo -H pip3 install -i ${PIP_SOURCE} -U tensorflow-gpu
+	
+	# install in python2
+	sudo apt -y  install ipython
+	sudo apt -y  install python-pip
+	sudo -H pip install -i ${PIP_SOURCE} -U pip
+	sudo apt -y remove python-pip
+	sudo -H pip install -i ${PIP_SOURCE} -U tensorflow-gpu
+	
+	echo 'check python2 dependency'
+	sudo -H pip install -i ${PIP_SOURCE} check
+	echo 'check python3 dependency'
+	sudo -H pip3 install -i ${PIP_SOURCE} check
+	
+fi
+
+
+
+echo "tensorflow-gpu installed in python2 and python3"
+echo "you can type ipython2 and ipython3 to test it"
